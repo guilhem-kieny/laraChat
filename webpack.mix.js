72ac7f01082df0c8578d511fcced1760f911af02
guilhemkieny <g.kieny@france-solar.fr>
2023-12-13 14:33:41 +0100

    header app & scss (little bug when refresh)2

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue({version:2})
-    .css('resources/css/app.css', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
     .version();
